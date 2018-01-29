91761104f480fb5cf4955aaede05bd7a34785c01
soywiz <soywiz@gmail.com>
2018-01-29 21:54:01 +0100

    Test demo route using hamcrest containsString

--- quickstart/src/test/kotlin/ApplicationTest.kt
+++ quickstart/src/test/kotlin/ApplicationTest.kt
@@ -5,17 +5,21 @@ import io.ktor.http.withCharset
 import io.ktor.server.testing.contentType
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.withTestApplication
+import org.hamcrest.CoreMatchers.containsString
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThat
 import org.junit.Test
 
 class ApplicationTest {
     @Test
-    fun testRequest() = withTestApplication({
-        module()
-    }) {
+    fun testRequest() = withTestApplication(io.ktor.application.Application::module) {
         with(handleRequest(HttpMethod.Get, "/")) {
             assertEquals(HttpStatusCode.OK, response.status())
             assertEquals("Hello, world!", response.content)
+        }
+        with(handleRequest(HttpMethod.Get, "/demo")) {
+            assertEquals(HttpStatusCode.OK, response.status())
+            assertThat(response.content, containsString("/user/user3"))
         }
         with(handleRequest(HttpMethod.Get, "/css/styles.css")) {
             assertEquals(HttpStatusCode.OK, response.status())
