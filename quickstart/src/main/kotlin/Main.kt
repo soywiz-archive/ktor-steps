5f2f2130d6c88c014cb3c49494e1497ac0b761bf
soywiz <soywiz@gmail.com>
2018-01-29 21:42:58 +0100

    Static files + Locations

--- quickstart/src/main/kotlin/Main.kt
+++ quickstart/src/main/kotlin/Main.kt
@@ -1,11 +1,15 @@
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.features.ConditionalHeaders
+import io.ktor.content.files
+import io.ktor.content.static
 import io.ktor.html.respondHtml
 import io.ktor.http.ContentType
+import io.ktor.locations.Locations
+import io.ktor.locations.get
+import io.ktor.locations.location
+import io.ktor.locations.locations
 import io.ktor.response.respondText
-import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
@@ -17,21 +21,29 @@ fun main(args: Array<String>) {
     }.apply {
         start(wait = true)
     }
-
 }
 
+@location("/")
+class IndexLocation()
+
+@location("/demo")
+class DemoLocation()
+
+@location("/user/{name}")
+class UserLocation(val name: String)
+
 fun Application.module() {
-    //install(Routing) {
-    //}
-    install(ConditionalHeaders) {
-
-    }
-
+    install(Locations)
     routing {
-        get("/") {
+        static("css") {
+            files("css")
+        }
+        //get("/") {
+        get<IndexLocation> {
             call.respondText("Hello, world!", ContentType.Text.Plain)
         }
-        get("/demo") {
+        //get("/demo") {
+        get<DemoLocation> {
             call.respondHtml {
                 head {
                     title("hello")
@@ -40,11 +52,20 @@ fun Application.module() {
                     ul {
                         for (n in 0 until 10) {
                             li {
-                                a("..") {
+                                a(locations.href(if (n % 2 == 0) DemoLocation() else UserLocation("user$n"))) {
                                     +"hello"
                                 }
                             }
                         }
+                    }
+                }
+            }
+        }
+        get<UserLocation> { location ->
+            call.respondHtml {
+                body {
+                    h1 {
+                        +location.name
                     }
                 }
             }
