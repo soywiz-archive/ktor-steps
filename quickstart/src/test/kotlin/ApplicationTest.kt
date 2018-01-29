43e0b458f91837c8aec5498120f9d6fe189adf17
soywiz <soywiz@gmail.com>
2018-01-29 21:45:49 +0100

    Check that styles.css returns the proper content-type

--- quickstart/src/test/kotlin/ApplicationTest.kt
+++ quickstart/src/test/kotlin/ApplicationTest.kt
@@ -1,5 +1,8 @@
+import io.ktor.http.ContentType
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.withCharset
+import io.ktor.server.testing.contentType
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.withTestApplication
 import org.junit.Assert.assertEquals
@@ -13,6 +16,10 @@ class ApplicationTest {
         with(handleRequest(HttpMethod.Get, "/")) {
             assertEquals(HttpStatusCode.OK, response.status())
             assertEquals("Hello, world!", response.content)
+        }
+        with(handleRequest(HttpMethod.Get, "/css/styles.css")) {
+            assertEquals(HttpStatusCode.OK, response.status())
+            assertEquals(ContentType.Text.CSS.withCharset(Charsets.UTF_8), response.contentType())
         }
     }
 }
